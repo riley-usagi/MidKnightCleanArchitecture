f7d806b8197d521baab3b2cff3283c36c2c444a0
Riley Usagi <riley.usagi@gmail.com>
2021-11-08 19:58:25 +0300

    Init

---
+++ MidKnight/MidKnightApp.swift
@@ -0,0 +1,17 @@
+//
+//  MidKnightApp.swift
+//  MidKnight
+//
+//  Created by Riley Usagi on 08.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct MidKnightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
