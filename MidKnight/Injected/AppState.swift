51581aebc6ad8b3001fce4bc899d86f9439d0bf7
Riley Usagi <riley.usagi@gmail.com>
2021-11-09 10:37:07 +0300

    Done

--- MidKnight/Injected/AppState.swift
+++ MidKnight/Injected/AppState.swift
@@ -3,3 +3,11 @@ import SwiftUI
 struct AppState: Equatable {
   var currentPage: Container.Routes = .loading
 }
+
+
+// Чтение данных о дате из UserDefaults
+////read
+//let date = UserDefaults.standard.object(forKey: key) as! Date
+//let df = DateFormatter()
+//df.dateFormat = "dd/MM/yyyy HH:mm"
+//print(df.string(from: date))
