51cb5150a8e4980d3839686ca1b5ded7f531c781
SIARHEI DUBKO <dolph-1@yandex.ru>
2021-11-09 14:51:56 +0300

    start create buttons

--- MidKnight/Screens/Today/TodayScreen.swift
+++ MidKnight/Screens/Today/TodayScreen.swift
@@ -1,35 +1,100 @@
 import SwiftyUserDefaults
 import SwiftUI
 
+enum CalcButton: String {
+  case one
+  case two
+  case three
+  case four
+  case five
+  case six
+  case seven
+  case eight
+  case nine
+  case zero
+  case clear
+  case dot
+  case enter
+}
+
 struct TodayScreen: View {
   
   @Environment(\.container) var container: Container
   
   @State private var todayCash = Defaults[\.todayCash]
   
+  let buttons: [[CalcButton]] = [
+    [.seven, .eight, .nine]
+  ]
+    
   var body: some View {
-    VStack {
-      Text("\(todayCash ?? 7) на сегодня")
-        .font(.largeTitle)
-        .padding()
+    
+    ZStack {
       
-      TextField("For today", value: $todayCash, formatter: NumberFormatter(), prompt: nil)
-        .onSubmit {
-          Defaults[\.todayCash] = todayCash
+      VStack(spacing: 0) {
+        
+        HStack {
+          Text("\(todayCash ?? 7) на сегодня")
+            .font(.largeTitle)
+            
+          Spacer()
+          
+          Button {
+            container.appState.value.currentPage = .settings
+          } label: {
+            Image(systemName: "gearshape.fill")
+              .resizable()
+              .frame(width: 30, height: 30)
+              .foregroundColor(.black)
+          }
+          .buttonStyle(DefaultButtonStyle())
         }
-        .keyboardType(.numberPad)
         .padding()
-      
-      
-      Spacer()
-      
-      Button {
-        container.appState.value.currentPage = .settings
-      } label: {
-        Text("To Settings")
-      }
-      .buttonStyle(DefaultButtonStyle())
+        
+        Divider()
+        
+        TextField("For today", value: $todayCash, formatter: NumberFormatter(), prompt: nil)
+          .onSubmit {
+            Defaults[\.todayCash] = todayCash
+          }
+          .keyboardType(.numberPad)
+          .padding()
+        
+        Divider()
+        
+        HStack {
+          Text("0")
+            .bold()
+            .font(.system(size: 52))
+            .foregroundColor(.black)
+            
+          Spacer()
+        }
+        .padding()
+        
+        ForEach(buttons, id: \.self) { row in
+          ForEach(row, id: \.self) { item in
+            Button {
+              
+            } label: {
+              Text(item.rawValue)
+                .frame(width: 70, height: 70)
+                .background(Color.orange)
+                .foregroundColor(.white)
+                .cornerRadius(35)
+            }
 
+          }
+        }
+        
+        
+        
+        
+        
+        
+        
+      }
+      .padding(.horizontal, 30)
     }
   }
 }
