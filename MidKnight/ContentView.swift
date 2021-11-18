79f06919ffe8fe2f976b6f617197f48414c49809
Riley Usagi <riley.usagi@gmail.com>
2021-11-18 13:36:59 +0300

    Clear

---
+++ MidKnight/ContentView.swift
@@ -0,0 +1,15 @@
+//
+//  ContentView.swift
+//  MidKnight
+//
+//  Created by Riley Usagi on 18.11.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
---
+++ MidKnight/MidKnightApp.swift
@@ -0,0 +1,17 @@
+//
+//  MidKnightApp.swift
+//  MidKnight
+//
+//  Created by Riley Usagi on 18.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct MidKnightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
